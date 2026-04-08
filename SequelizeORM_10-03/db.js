87dddfd65264147ad0bbd9a51fd4c0f887df4dc2
João Vitor dos Santos Ritter <joao.ritter@ufn.edu.br>
2026-04-07 23:35:55 -0300

    criacao do front end com next.js

--- SequelizeORM_10-03/db.js
+++
@@ -1,9 +0,0 @@
-import Sequelize from 'sequelize';
-
-const sequelize = new Sequelize('crud_sequelize', 'root', 'www.com.brj', {
-    host: '127.0.0.1',
-    dialect: 'mysql'
-}
-);
-
-export default sequelize;
